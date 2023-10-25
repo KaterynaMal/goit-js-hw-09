52986c35294d3afd6bf1885dd014c897218411df
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-25 15:10:48 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerId = null;
+startButton.addEventListener('click', () => {
+  if (timerId) {
+    return;
+  }
+
+  startButton.disabled = true;
+
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  timerId = null;
+  startButton.disabled = false;
+});
